4cb72fbe1fa5c2290a1eb2d61c090aaf224cfd53
andress <nithson.barragan.03@gmail.com>
2025-03-06 10:47:25 -0500

    actualizado 7

--- src/components/Principal.vue
+++ src/components/Principal.vue
@@ -4,13 +4,14 @@
 
     <nav class="navbar navbar-dark bg-dark fixed-top">
   <div class="container-fluid">
-    <a class="navbar-brand" style="font-size: 30px;" href="#">SNEAKERS KENI👽</a>
+    <a class="navbar-brand" style="font-size: 30px;font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;" 
+    href="#">SNEAKERS KENI</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
       <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">¿Te gustó algun modelo?⬇️🔥</h5>
+        <h5 style="font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;" class="offcanvas-title" id="offcanvasDarkNavbarLabel">¿TE GUSTÓ ALGUN MODEDELO?⬇</h5>
         <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
       </div>
       <div class="offcanvas-body">
@@ -19,24 +20,33 @@
       
 
           <li class="nav-item">
-            <p class="nav-link" href="#">PRESIONA EL ICONO Y CONTACTANOS:</p>
 
-            Kevin🥷🏽
+            
             <a href="https://wa.me/573204084054?text=Hola kevin,%20quiero%20más%20información" target="_blank">
-            <img style="height: 40px; width: 40px; margin:5px;" src="../assets/wsp.png" alt="">
+            <!-- From Uiverse.io by NelsonDJCR --> 
+<button class="button2">
+    KEVIN
+    <svg viewBox="0 0 48 48" y="0px" x="0px" xmlns="http://www.w3.org/2000/svg">
+<path d="M4.868,43.303l2.694-9.835C5.9,30.59,5.026,27.324,5.027,23.979C5.032,13.514,13.548,5,24.014,5c5.079,0.002,9.845,1.979,13.43,5.566c3.584,3.588,5.558,8.356,5.556,13.428c-0.004,10.465-8.522,18.98-18.986,18.98c-0.001,0,0,0,0,0h-0.008c-3.177-0.001-6.3-0.798-9.073-2.311L4.868,43.303z" fill="#fff"></path><path d="M4.868,43.803c-0.132,0-0.26-0.052-0.355-0.148c-0.125-0.127-0.174-0.312-0.127-0.483l2.639-9.636c-1.636-2.906-2.499-6.206-2.497-9.556C4.532,13.238,13.273,4.5,24.014,4.5c5.21,0.002,10.105,2.031,13.784,5.713c3.679,3.683,5.704,8.577,5.702,13.781c-0.004,10.741-8.746,19.48-19.486,19.48c-3.189-0.001-6.344-0.788-9.144-2.277l-9.875,2.589C4.953,43.798,4.911,43.803,4.868,43.803z" fill="#fff"></path><path d="M24.014,5c5.079,0.002,9.845,1.979,13.43,5.566c3.584,3.588,5.558,8.356,5.556,13.428c-0.004,10.465-8.522,18.98-18.986,18.98h-0.008c-3.177-0.001-6.3-0.798-9.073-2.311L4.868,43.303l2.694-9.835C5.9,30.59,5.026,27.324,5.027,23.979C5.032,13.514,13.548,5,24.014,5 M24.014,42.974C24.014,42.974,24.014,42.974,24.014,42.974C24.014,42.974,24.014,42.974,24.014,42.974 M24.014,42.974C24.014,42.974,24.014,42.974,24.014,42.974C24.014,42.974,24.014,42.974,24.014,42.974 M24.014,4C24.014,4,24.014,4,24.014,4C12.998,4,4.032,12.962,4.027,23.979c-0.001,3.367,0.849,6.685,2.461,9.622l-2.585,9.439c-0.094,0.345,0.002,0.713,0.254,0.967c0.19,0.192,0.447,0.297,0.711,0.297c0.085,0,0.17-0.011,0.254-0.033l9.687-2.54c2.828,1.468,5.998,2.243,9.197,2.244c11.024,0,19.99-8.963,19.995-19.98c0.002-5.339-2.075-10.359-5.848-14.135C34.378,6.083,29.357,4.002,24.014,4L24.014,4z" fill="#cfd8dc"></path><path d="M35.176,12.832c-2.98-2.982-6.941-4.625-11.157-4.626c-8.704,0-15.783,7.076-15.787,15.774c-0.001,2.981,0.833,5.883,2.413,8.396l0.376,0.597l-1.595,5.821l5.973-1.566l0.577,0.342c2.422,1.438,5.2,2.198,8.032,2.199h0.006c8.698,0,15.777-7.077,15.78-15.776C39.795,19.778,38.156,15.814,35.176,12.832z" fill="#40c351"></path><path clip-rule="evenodd" d="M19.268,16.045c-0.355-0.79-0.729-0.806-1.068-0.82c-0.277-0.012-0.593-0.011-0.909-0.011c-0.316,0-0.83,0.119-1.265,0.594c-0.435,0.475-1.661,1.622-1.661,3.956c0,2.334,1.7,4.59,1.937,4.906c0.237,0.316,3.282,5.259,8.104,7.161c4.007,1.58,4.823,1.266,5.693,1.187c0.87-0.079,2.807-1.147,3.202-2.255c0.395-1.108,0.395-2.057,0.277-2.255c-0.119-0.198-0.435-0.316-0.909-0.554s-2.807-1.385-3.242-1.543c-0.435-0.158-0.751-0.237-1.068,0.238c-0.316,0.474-1.225,1.543-1.502,1.859c-0.277,0.317-0.554,0.357-1.028,0.119c-0.474-0.238-2.002-0.738-3.815-2.354c-1.41-1.257-2.362-2.81-2.639-3.285c-0.277-0.474-0.03-0.731,0.208-0.968c0.213-0.213,0.474-0.554,0.712-0.831c0.237-0.277,0.316-0.475,0.474-0.791c0.158-0.317,0.079-0.594-0.04-0.831C20.612,19.329,19.69,16.983,19.268,16.045z" fill-rule="evenodd" fill="#fff"></path>
+</svg>
+</button>
             </a>
 
-            <br>
+            <br><br>
 
-            Andres🥷🏽
+            
             <a href="https://wa.me/573209025487?text=Hola Andres,%20quiero%20más%20información" target="_blank">
-            <img style="height: 40px; width: 40px; margin:5px;" src="../assets/wsp.png" alt="">
+            <!-- From Uiverse.io by NelsonDJCR --> 
+<button class="button2">
+    ANDRES
+    <svg viewBox="0 0 48 48" y="0px" x="0px" xmlns="http://www.w3.org/2000/svg">
+<path d="M4.868,43.303l2.694-9.835C5.9,30.59,5.026,27.324,5.027,23.979C5.032,13.514,13.548,5,24.014,5c5.079,0.002,9.845,1.979,13.43,5.566c3.584,3.588,5.558,8.356,5.556,13.428c-0.004,10.465-8.522,18.98-18.986,18.98c-0.001,0,0,0,0,0h-0.008c-3.177-0.001-6.3-0.798-9.073-2.311L4.868,43.303z" fill="#fff"></path><path d="M4.868,43.803c-0.132,0-0.26-0.052-0.355-0.148c-0.125-0.127-0.174-0.312-0.127-0.483l2.639-9.636c-1.636-2.906-2.499-6.206-2.497-9.556C4.532,13.238,13.273,4.5,24.014,4.5c5.21,0.002,10.105,2.031,13.784,5.713c3.679,3.683,5.704,8.577,5.702,13.781c-0.004,10.741-8.746,19.48-19.486,19.48c-3.189-0.001-6.344-0.788-9.144-2.277l-9.875,2.589C4.953,43.798,4.911,43.803,4.868,43.803z" fill="#fff"></path><path d="M24.014,5c5.079,0.002,9.845,1.979,13.43,5.566c3.584,3.588,5.558,8.356,5.556,13.428c-0.004,10.465-8.522,18.98-18.986,18.98h-0.008c-3.177-0.001-6.3-0.798-9.073-2.311L4.868,43.303l2.694-9.835C5.9,30.59,5.026,27.324,5.027,23.979C5.032,13.514,13.548,5,24.014,5 M24.014,42.974C24.014,42.974,24.014,42.974,24.014,42.974C24.014,42.974,24.014,42.974,24.014,42.974 M24.014,42.974C24.014,42.974,24.014,42.974,24.014,42.974C24.014,42.974,24.014,42.974,24.014,42.974 M24.014,4C24.014,4,24.014,4,24.014,4C12.998,4,4.032,12.962,4.027,23.979c-0.001,3.367,0.849,6.685,2.461,9.622l-2.585,9.439c-0.094,0.345,0.002,0.713,0.254,0.967c0.19,0.192,0.447,0.297,0.711,0.297c0.085,0,0.17-0.011,0.254-0.033l9.687-2.54c2.828,1.468,5.998,2.243,9.197,2.244c11.024,0,19.99-8.963,19.995-19.98c0.002-5.339-2.075-10.359-5.848-14.135C34.378,6.083,29.357,4.002,24.014,4L24.014,4z" fill="#cfd8dc"></path><path d="M35.176,12.832c-2.98-2.982-6.941-4.625-11.157-4.626c-8.704,0-15.783,7.076-15.787,15.774c-0.001,2.981,0.833,5.883,2.413,8.396l0.376,0.597l-1.595,5.821l5.973-1.566l0.577,0.342c2.422,1.438,5.2,2.198,8.032,2.199h0.006c8.698,0,15.777-7.077,15.78-15.776C39.795,19.778,38.156,15.814,35.176,12.832z" fill="#40c351"></path><path clip-rule="evenodd" d="M19.268,16.045c-0.355-0.79-0.729-0.806-1.068-0.82c-0.277-0.012-0.593-0.011-0.909-0.011c-0.316,0-0.83,0.119-1.265,0.594c-0.435,0.475-1.661,1.622-1.661,3.956c0,2.334,1.7,4.59,1.937,4.906c0.237,0.316,3.282,5.259,8.104,7.161c4.007,1.58,4.823,1.266,5.693,1.187c0.87-0.079,2.807-1.147,3.202-2.255c0.395-1.108,0.395-2.057,0.277-2.255c-0.119-0.198-0.435-0.316-0.909-0.554s-2.807-1.385-3.242-1.543c-0.435-0.158-0.751-0.237-1.068,0.238c-0.316,0.474-1.225,1.543-1.502,1.859c-0.277,0.317-0.554,0.357-1.028,0.119c-0.474-0.238-2.002-0.738-3.815-2.354c-1.41-1.257-2.362-2.81-2.639-3.285c-0.277-0.474-0.03-0.731,0.208-0.968c0.213-0.213,0.474-0.554,0.712-0.831c0.237-0.277,0.316-0.475,0.474-0.791c0.158-0.317,0.079-0.594-0.04-0.831C20.612,19.329,19.69,16.983,19.268,16.045z" fill-rule="evenodd" fill="#fff"></path>
+</svg>
+</button>
             </a>
             
             <br>
-            Instagram Oficial
-            <img style="height: 40px; width: 40px; margin:5px;" src="../assets/instagram.png" alt="">
-
+        
           </li>
    
         
@@ -59,11 +69,31 @@
   </div>
 </nav>
 
-<br><br><br><br>
+<br><br><br>
 
 
 <center>
-  <img class="imagenkeni" src="../assets/tenis.webp" alt="">
+  <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="../assets/nikeuno.jpg" class="carrouno d-block w-90" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="../assets/nikeuno.jpg" class="carrouno d-block w-90" alt="...">    
+    </div>
+    <div class="carousel-item">
+      <img src="../assets/nikeuno.jpg" class="carrouno d-block w-90" alt="...">    
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
 </center>
 
 <br><br><br>
@@ -73,23 +103,43 @@
 
 
 
- 
 
-      <div v-for="producto in productos" :key="producto._id">
-        <div class="card">
-          <img :src="producto.imagen" class="card-img-top" alt="Producto" />
-          <div class="card-body">
-            <center>
-                 <p class="card-text">
-            PRODUCTO: {{ producto.nombre }} <br>
-            PRECIO: {{ producto.precio }}💸</p>
-            </center>
-         
-          </div>
+
+ 
+  <div v-for="producto in productos" :key="producto._id">
+    <div class="card">
+      <!-- Spinner mientras la imagen carga -->
+      <div v-if="producto.cargando" class="spinner-container">
+        <div class="spinner-border text-light" role="status">
+          <span class="visually-hidden">Loading...</span>
         </div>
       </div>
 
-  
+      <!-- Imagen del producto -->
+      <img
+        v-show="!producto.cargando"
+        :src="producto.imagen"
+        class="card-img-top"
+        alt="Producto"
+        @load="producto.cargando = false"
+      />
+
+      <div class="card-body">
+        <center>
+          <div>
+            {{ producto.nombre }} <br />
+            {{ new Intl.NumberFormat("es-CO", { style: "currency", currency: "COP" }).format(producto.precio) }}
+          </div>
+        </center>
+      </div>
+    </div>
+  </div>
+
+
+
+
+
+
 
  
 </div>
@@ -106,6 +156,16 @@
 
 
 <style>
+
+
+.carrouno{
+height: 580px;
+width: 100%;
+}
+
+
+/* ..... */
+
   .imagenkeni {
     height: 60vh;
     width: 40vw;
@@ -113,21 +173,32 @@
 
   body {
     background-color: #000000;
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
   }
 
   .cards-container {
     display: flex;
     flex-wrap: wrap;
-    gap: 16px; /* Espacio entre tarjetas */
+    gap: 40px; /* Espacio entre tarjetas */
     justify-content: center; /* Centra las tarjetas en la fila */
   }
 
   .card {
+
     background-color: #000000;
-    
     width: 18rem; /* Ajusta el tamaño de la tarjeta */
     color: #fff;
+    transition: transform 0.3s ease-in-out, box-shadow 0.3s ease-in-out;
+
   }
+
+
+  .card:hover {
+  transform: translateY(-8px) scale(1.05);
+  box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.2);
+}
+
+  
 
   /* Media query para pantallas más pequeñas como iPhone 14 Pro Max */
   @media screen and (max-width: 431px) {
@@ -146,34 +217,119 @@
       width: 80%; /* Hace que la imagen ocupe todo el ancho disponible */
       height: auto; /* Mantiene la proporción de la imagen */
     }
+
+    .carrouno{
+height: 300px;
+width: 100%;
+}
   }
+
+
+
+
+/* From Uiverse.io by NelsonDJCR */ 
+.button2 {
+  display: inline-block;
+  transition: all 0.2s ease-in;
+  position: relative;
+  overflow: hidden;
+  z-index: 1;
+  color: #090909;
+  padding: 0.7em 3.3em 0.7em 1.7em;
+  font-size: 18px;
+  border-radius: 0.5em;
+  background: #e8e8e8;
+  border: 1px solid #e8e8e8;
+
+}
+
+.button2 > svg {
+  height: 30px;
+  position: absolute;
+  padding: 0px 5px;
+  margin-top: -4px;
+}
+
+.button2:active {
+  color: #666;
+  box-shadow: inset 4px 4px 12px #c5c5c5,
+             inset -4px -4px 12px #ffffff;
+}
+
+.button2:before {
+  content: "";
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%) scaleY(1) scaleX(1.25);
+  top: 100%;
+  width: 140%;
+  height: 180%;
+  background-color: rgba(0, 0, 0, 0.05);
+  border-radius: 50%;
+  display: block;
+  transition: all 0.5s 0.1s cubic-bezier(0.55, 0, 0.1, 1);
+  z-index: -1;
+}
+
+.button2:after {
+  content: "";
+  position: absolute;
+  left: 55%;
+  transform: translateX(-50%) scaleY(1) scaleX(1.45);
+  top: 180%;
+  width: 160%;
+  height: 190%;
+  background-color: #009087;
+  border-radius: 50%;
+  display: block;
+  transition: all 0.5s 0.1s cubic-bezier(0.55, 0, 0.1, 1);
+  z-index: -1;
+}
+
+.button2:hover {
+  color: #ffffff;
+  border: 1px solid #009087;
+}
+
+.button2:hover:before {
+  top: -35%;
+  background-color: #009087;
+  transform: translateX(-50%) scaleY(1.3) scaleX(0.8);
+}
+
+.button2:hover:after {
+  top: -45%;
+  background-color: #009087;
+  transform: translateX(-50%) scaleY(1.3) scaleX(0.8);
+}
+
+
 </style>
 
 
 
 <script setup>
-
 import { ref, onMounted } from "vue";
 import axios from "axios";
 
-const productos = ref([]); // Almacenará los productos
+const productos = ref([]);
 
 
-// Función para obtener los productos desde el backend
+// Función para obtener los productos desde la API
 const obtenerProductos = async () => {
   try {
     const respuesta = await axios.get("https://tenis-7hv3.onrender.com/productos");
-    
-   productos.value = respuesta.data;
- 
+
+    // Agregar la propiedad 'cargando' a cada producto para que sea reactiva
+    productos.value = respuesta.data.map((producto) => ({
+      ...producto,
+      cargando: true, // Inicia en "cargando"
+    }));
+
   } catch (error) {
     console.error("Error al obtener productos", error);
   }
-  console.log(productos.value);
-  
 };
-
-  
 
 // Ejecutar la función al montar el componente
 onMounted(obtenerProductos);
